d939ac135e181c9aeb3342bb13ab337f6cef497d
jana <kamalgana2004@gmail.com>
2025-04-15 15:20:56 +0200

    <3

---
+++ arbres/src/Main.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args){
+    Tree l1 = new Tree(1);
+    Tree l3 = new Tree(3);
+    Tree l5 = new Tree(5);
+    Tree l7 = new Tree(7);
+    
+    Tree t2 = new Tree(2, new Tree[]{l1, l3});
+    Tree t3 = new Tree(2, new Tree[]{l1,l3,l5});
+    Tree t6 = new Tree(6, new Tree[]{l7});
+    
+    Tree t4 = new Tree(4, new Tree[]{t2, l5, t6});
+
+    // 4 2 5 6 1 3 7
+
+    System.out.println(Trees.nbrLeaves(t4));
+    
+    
+    Tree[] ls = Trees.flattenLeaves(t4);
+    System.out.println("Les " + ls.length + " feuilles");
+    int i = 0;
+    while(i != ls.length) {
+      System.out.println(ls[i].getValue());
+      i++;
+    }
+
+    System.out.println("Path V1");    
+    Trees.pathV1(l7);
+
+    System.out.println("Path V2");    
+    Trees.pathV2(l7);
+
+    System.out.println("test nombre de noeud");
+    System.out.println(Trees.nbrNode(t2));
+
+    System.out.println("test trouver le min");
+    System.out.println(Trees.min(t2));
+
+    System.out.println("test trouver la somme");
+    System.out.println(Trees.sum(t2));
+
+    System.out.println("test si les arbres sont egaux");
+    System.out.println(Trees.equals(t3,t3));
+
+    System.out.println("test la profondeur");
+    System.out.println(Trees.depth(l7));
+
+    System.out.println("test mm arbre ou pas");
+    System.out.println(Trees.sameOne(t2,l3));
+
+    System.out.println("test dfs");
+    Trees.dfsPrint(t4);
+
+    System.out.println("test bfs");
+    Tree t1 = new Tree(1, new Tree[]{new Tree(8)});
+    Tree t2b = new Tree(2, new Tree[]{t1, l3});
+    Tree t4b = new Tree(4, new Tree[]{t2b, l5, t6});
+    Trees.bfsPrint(t4b);
+
+    System.out.println("test print Path v1");
+    Trees.printPathV1(l7);
+
+    System.out.println("test print Path v2");
+    Trees.printPathV2(l7);
+
+    System.out.println("test print Path v3");
+    Trees.printPathV3(t6, 7);
+
+    System.out.println("test toArray :");
+    int[][] mat = Trees.toArray(t4);
+    System.out.println(Arrays.toString(mat[0]));
+    System.out.println(Arrays.toString(mat[1]));
+    System.out.println(Arrays.toString(mat[2]));
+  }
+}
---
+++ arbres/src/Trees.java
@@ -0,0 +1,250 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Trees {
+
+	// *******************************************************
+	// Un premier exemple: le nombre de feuilles d'un arbre
+	// *******************************************************
+	public static int nbrLeaves(Tree t) {
+		int r;
+		if (t.isLeaf()) {
+			r = 1;
+		} else {
+			r = 0;
+			for (Tree child : t) {
+				r += nbrLeaves(child);
+			}
+		}
+		return r;
+	}
+
+	// *******************************************************
+	// Un deuxième exemple: aplanir un arbre
+	// *******************************************************
+	public static Tree[] flattenLeaves(Tree t) {
+		int nl = nbrLeaves(t);
+		Tree[] r = new Tree[nl];
+		flattenLeaves(t, r, 0);
+		return r;
+	}
+
+	private static int flattenLeaves(Tree t, Tree[] a, int idx) {
+		int r;
+		if (t.isLeaf()) {
+			a[idx] = t;
+			r = 1;
+		} else {
+			r = 0;
+			for (Tree child : t) {
+				r += flattenLeaves(child, a, idx + r);
+			}
+		}
+		return r;
+	}
+
+	// *******************************************************
+	// Un troisième exemple:
+	// tous les algorithmes ne sont pas récursifs
+	// *******************************************************
+	public static void pathV1(Tree t) {
+		System.out.println(t.getValue());
+		if (t.getParent() != null) {
+			pathV1(t.getParent());
+		}
+	}
+
+	public static void pathV2(Tree t) {
+		while (t != null) {
+			System.out.println(t.getValue());
+			t = t.getParent();
+		}
+	}
+
+	// *******************************************************
+	// Exercices 1
+	// *******************************************************
+
+	// 1.1)
+
+	public static int nbrNode(Tree t) {
+		if(t==null) return 0;
+		int nbrNoeud=1;
+		for(Tree child: t.getChildren()){
+			nbrNoeud+=nbrNode(child);
+		}
+		return nbrNoeud;
+	}
+
+
+	// 1.2)
+	public static int min(Tree t) {
+		if(t==null) return Integer.MAX_VALUE;
+		int min= t.getValue();
+		for(Tree child: t.getChildren()){
+			int childMin = min(child);
+			if(min>childMin){
+				min=childMin;
+			}
+		}
+		return min;
+	}
+
+	// 1.3)
+	public static int sum(Tree t) {
+		if(t==null) return 0;
+		int somme=t.getValue();
+		for(Tree child: t.getChildren()){
+			somme+=sum(child);
+		}
+		return somme;
+	}
+
+	// 1.4)
+	public static boolean equals(Tree t1, Tree t2) {
+		if (t1 == null || t2 == null) return t1 == t2;
+		if (t1.getValue() != t2.getValue()) return false;
+		Tree[] c1 = t1.getChildren();
+		Tree[] c2 = t2.getChildren();
+		if (c1.length != c2.length) return false;
+		for (int i = 0; i < c1.length; i++) {
+			if (!equals(c1[i], c2[i])) return false;
+		}
+
+		return true;
+	}
+
+	// 1.5)
+	public static int depth(Tree n) {
+		if (n == null || n.getParent() == null) return 0;
+		return 1 + depth(n.getParent());
+	}
+
+	// 1.6)
+	public static boolean sameOne(Tree n1, Tree n2) {
+		if(n1.getParent()==null && n2.getParent()!=null){
+			return sameOne(n1, n2.getParent());
+		}
+		if(n1.getParent()!=null && n2.getParent()==null){
+			return sameOne(n1.getParent(),n2);
+		}
+		if(n1.getParent()==null && n2.getParent()==null){
+			if(n1==n2) return true;
+			else return false;
+		}
+		return sameOne(n1.getParent(),n2.getParent());
+	}
+
+	// 1.7)
+	public static void dfsPrint(Tree t) {
+		System.out.println(t.getValue());
+		Tree[] children = t.getChildren();
+		for (Tree child : children) {
+			dfsPrint(child);
+		}
+	}
+
+	// 1.8)
+	public static void bfsPrint(Tree t) {
+		if (t == null) return;
+		Queue<Tree> q = new LinkedList<Tree>();
+		q.add(t);
+		bfsPrintBis(q);
+	}
+
+	private static void bfsPrintBis(Queue<Tree> q) {
+		if (q.isEmpty()) return;
+		Tree current = q.poll();
+		System.out.println(current.getValue());
+		for (Tree child : current.getChildren()) {
+			q.add(child);
+		}
+		bfsPrintBis(q);
+	}
+
+
+	// *******************************************************
+	// Exercices 2
+	// *******************************************************
+
+	// 2.1)
+	static void printPathV1(Tree node) {
+		if (node == null) return;
+		printPathV1(node.getParent());
+		System.out.println(node.getValue());
+	}
+
+	// 2.2)
+	static void printPathV2(Tree node) {
+		Deque<Tree> pile = new ArrayDeque<>();
+		Tree current = node;
+
+		while (current != null) {
+			pile.push(current);
+			current = current.getParent();
+		}
+
+		while (!pile.isEmpty()) {
+			System.out.println(pile.pop().getValue());
+		}
+	}
+
+	// 2.3
+	static void printPathV3(Tree t, int v) {
+		if (t.getValue() == v) System.out.println(t.getValue());
+
+		for (Tree child : t.getChildren()) {
+			System.out.println(t.getValue());
+			printPathV3(child, v);
+		}
+
+	}
+
+	// *******************************************************
+	// Exercices 3
+	// *******************************************************
+
+	// 3.1
+	public static int[][] toArray(Tree t) {
+		int n = nbrLeaves(t);
+		int[][] mat = new int[3][n+1];
+		fillMatrix(t, mat, 0);
+		return mat;
+	}
+
+	private static void fillMatrix(Tree t, int[][] mat, int index) {
+		int currentIndex = index;
+		mat[0][currentIndex] = t.getValue();
+		index++;
+		int leftIndex = -1;
+		int rightIndex = -1;
+		if (t.getChildren().length > 0) {
+			leftIndex = index;
+			fillMatrix(t.getChildren()[0], mat, index);
+		}
+
+		if (t.getChildren().length > 1) {
+			rightIndex = index;
+			fillMatrix(t.getChildren()[1], mat, index);
+		}
+		mat[1][currentIndex] = leftIndex;
+		mat[2][currentIndex] = rightIndex;
+
+	}
+
+
+	// 3.2
+	public static Tree toTree(int[][] t) {
+		return null;
+	}
+
+	// *******************************************************
+	// Exercices 4
+	// *******************************************************
+
+	public static Tree lca(Tree n1, Tree n2) {
+		return null;
+	}
+}
